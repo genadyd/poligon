7f65107d5bfc50f36117e8f1e0dfd2bf4e0a2382
genady <jobgenady@gmail.com>
2020-12-01 07:56:25 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ URL::asset('favicon.png') }}" type="image/x-icon"/>
+    <title>Laravel poligon</title>
+</head>
+<body>
+<h1>Laravel poligon</h1>
+</body>
+</html>
